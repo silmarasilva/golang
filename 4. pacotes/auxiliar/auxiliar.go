783176355ec84d754e393409e490f71e01f42abb
silmarasilva <silmara.eliza@gmail.com>
2023-10-11 18:44:44 -0300

    feat: pacotes

--- 4. pacotes/auxiliar/auxiliar.go
+++ 4. pacotes/auxiliar/auxiliar.go
@@ -2,6 +2,10 @@
 // se uma função começa com letra minúscula, ela só vai ser visível dentro do pacote que ela está.
 // se começa com letra maiúscula, ela poderá ser exportada por outros pacotes. Isso é bem particular do Go.
 // é uma boa prática ter um comentário sobre uma função exportadas explicando o que ela faz. Se não for feito o comentário ficará gerando um warning, mas funcionará normalmente.
+// dentro do próprio pacote eu consigo referenciar as funções em letra minúscula, mas fora dele não.
+// O modo não se atualiza automaticamente, para atualizá-lo, rodar o comando <go build> novamente.
+// o comando <go install> salva o arquivo na raiz onde vc instalou o go ao invez de ficar dentro do projeto. Então usamos normalmente o <go build>
+
 package auxiliar
 
 // Escrever registra uma mensagem na tela.
@@ -9,4 +13,5 @@ import "fmt"
 
 func Escrever() {
 	fmt.Println("Escrevendo do pacote auxiliar")
+	escrever2()
 }
---
+++ 4. pacotes/auxiliar/auxiliar2.go
@@ -0,0 +1,7 @@
+package auxiliar
+
+import "fmt"
+
+func escrever2() {
+	fmt.Println("Escrevendo pela funação escrever 2")
+}
